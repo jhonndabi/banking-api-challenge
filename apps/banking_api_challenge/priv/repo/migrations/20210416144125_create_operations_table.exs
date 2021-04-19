84de7ba26df1949a2b9acd1a2445de0c177506c7
jhonndabi <jhonndabi.s.n@hotmail.com>
2021-04-19 14:24:43 -0300

    chore: rename account in-out to source-target (#7)

--- apps/banking_api_challenge/priv/repo/migrations/20210416144125_create_operations_table.exs
+++ apps/banking_api_challenge/priv/repo/migrations/20210416144125_create_operations_table.exs
@@ -7,13 +7,13 @@ defmodule BankingApiChallenge.Repo.Migrations.CreateOperationsTable do
       add :operation_type, :string, null: false
       add :amount, :integer, null: false, default: 0
 
-      add :account_in_id, references(:accounts, type: :uuid)
-      add :account_out_id, references(:accounts, type: :uuid)
+      add :account_source_id, references(:accounts, type: :uuid)
+      add :account_target_id, references(:accounts, type: :uuid)
 
       timestamps()
     end
 
-    create index(:operations, [:account_in_id, :account_out_id])
+    create index(:operations, [:account_target_id, :account_source_id])
     create constraint(:operations, :amount_nonnegative, check: "amount >= 0")
   end
 end
