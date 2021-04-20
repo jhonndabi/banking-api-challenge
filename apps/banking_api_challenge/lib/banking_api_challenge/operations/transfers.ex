6b6917f41690de1db478c76865378ed4f2c9a53b
Jonadabe Nascimento <jhonndabi.s.n@gmail.com>
2021-04-20 18:19:08 -0300

    chore: refactor transfer operation

--- apps/banking_api_challenge/lib/banking_api_challenge/operations/transfers.ex
+++ apps/banking_api_challenge/lib/banking_api_challenge/operations/transfers.ex
@@ -6,25 +6,10 @@ defmodule BankingApiChallenge.Operations.Transfers do
   alias BankingApiChallenge.Repo
 
   def transfer(%TransferInput{} = input) do
-    params = Map.from_struct(input)
-
-    with %{valid?: true} <- TransferInput.changeset(params),
-         {:ok, operation} <-
-           do_transfer(input.source_account_id, input.target_account_id, input.amount) do
-      {:ok, operation}
-    else
-      %{valid?: false} = changeset -> {:error, changeset}
-      {:error, reason} -> {:error, reason}
-    end
-  end
-
-  defp do_transfer(source_account_id, target_account_id, amount)
-       when is_integer(amount) and amount > 0 do
     fn ->
-      with {:ok, source_account} <- Accounts.get_account_with_lock(source_account_id),
-           {:ok, target_account} <- Accounts.get_account_with_lock(target_account_id),
-           {:ok, operation} <-
-             build_transfer_operation(target_account, source_account, amount) |> Repo.insert(),
+      with {:ok, source_account} <- Accounts.get_account_with_lock(input.source_account_id),
+           {:ok, target_account} <- Accounts.get_account_with_lock(input.target_account_id),
+           {:ok, operation} <- create_transfer_operation(target_account, source_account, input.amount),
            {:ok, _account} <- Accounts.decrease_balance(source_account, operation),
            {:ok, _account} <- Accounts.increase_balance(target_account, operation) do
         operation
@@ -33,14 +18,23 @@ defmodule BankingApiChallenge.Operations.Transfers do
       end
     end
     |> Repo.transaction()
+    |> case do
+      {:ok, operation} ->
+        {:ok, operation}
+
+      {:error, reason} ->
+        {:error, reason}
+    end
   end
 
-  defp build_transfer_operation(%Account{} = source_account, %Account{} = target_account, amount) do
-    Operation.changeset(%{
+  defp create_transfer_operation(%Account{} = source_account, %Account{} = target_account, amount) do
+    %{
       operation_type: "transfer",
       source_account_id: source_account.id,
       target_account_id: target_account.id,
       amount: amount
-    })
+    }
+    |> Operation.changeset()
+    |> Repo.insert()
   end
 end
